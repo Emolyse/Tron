fe783edd817e20782535302398427980f202e9ec
Rémy <drouet.remy@gmail.com>
2015-11-13 10:47:56 +0100

    server & dependencies update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-/**
- * Created by Rémy on 12/11/2015.
- */
+var express = require('express.io');
+var app = express();
+app.http().io();
+
+app.use(express.cookieParser());
+app.use(express.session({secret:'emolyse secret_key'}));
+
+app.io.route('evenement', function (requete) {
+
+});
+
+app.listen(3001, function () {
+
+});
