895177d5f123524a15a92f9fac0dfc767990b1cf
unknown <arn.alizee@gmail.com>
2015-11-16 16:52:45 +0100

    Mise en place de la connexion client

--- client/js/client.js
+++ client/js/client.js
@@ -1,3 +1,20 @@
 /**
  * Created by Rémy on 13/11/2015.
  */
+
+io = io.connect();
+
+/** Identification du joueur**/
+var joueur = localStorage['pseudo'];
+function login(){
+    io.emit("login",document.login.pseudo.value);
+}
+if(!joueur){
+    document.body.append("" +
+    "<overlay><form name='login' onsubmit='login()'>" +
+    "<input type='text' name='pseudo' placeholder='Pseudo'><input type='submit' value='Jouer'/>"+
+    "</form></overlay>");
+} else {
+    io.emit("login",joueur);
+}
+
--- index.js
+++ index.js
@@ -2,13 +2,24 @@ var express = require('express.io');
 var app = express();
 app.http().io();
 
-app.use(express.cookieParser());
-app.use(express.session({secret:'emolyse secret_key'}));
+//app.use(express.cookieParser());
+//app.use(express.session({secret:'emolyse secret_key'}));
 
-app.io.route('evenement', function (requete) {
+app.get('/', function (req,res) {
+    res.sendfile("client/index.html");
+});
+app.get('/client_script', function (req,res) {
 
+    res.sendfile("client/js/client.js");
 });
 
-app.listen(3001, function () {
+//app.io.route('ready', function (req) {
+//    req.io.respond("Coucou toi !");
+//    req.io.emit("repnewuser","c'est encore moi");
+//    req.io.broadcast("newuser","Yo les gars ya un nouveau! Il vous dis :"+req.data);
+//    app.io.broadcast("relou","Je fais chier tout le monde !")
+//});
 
+app.listen(3001, function () {
+    console.log("Listening localhost:3001");
 });
